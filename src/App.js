b77ba3d5f62e218620c712d0191494b233044f2b
tatianasnook <tatsnook@gmail.com>
2023-12-21 14:09:28 -0600

    change to video

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <video autoPlay muted loop>
+        <video autoPlay muted loop playsInline>
           <source src={video} type="video/mp4" />
         </video>
         <h1>Find a Recipe</h1>
